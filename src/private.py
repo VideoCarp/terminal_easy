e8f628d876389d210a3019343d7bab5e09ebee74
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2022-05-16 20:48:24 +1000

    Create private.py

---
+++ src/private.py
@@ -0,0 +1,7 @@
+# Private. You don't need to touch this.
+# If contributing, these are useful for removing global scope and reducing conflicts.
+# This is a *procedural*, modular CLI package, not an object-oriented one.
+progress = 0
+suffix = '%'
+sfx_len = len(suffix)
+to = 100
